e6e070313f5e5524dbd1b5218d2fc9323b5437f5
Aji9797 <23028757-Aji9797@users.noreply.replit.com>
2023-05-21 13:14:14 +0000

    Initial commit

---
+++ Exercise 2 - BMI Calculator.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+a=float (height)
+b=int (weight)
+c=str("BMI")
+c=b/(a)**2
+d=int(c)
+
+print(d)
---
+++ main.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#String
+print("Hello"[4])
+print("123" + "345")
+
+#Integer
+print(123 + 345)
+print (123_456_789)
+
+#Floating
+print(3.45679)
+
+#Boolean
+True
+False
+
+
